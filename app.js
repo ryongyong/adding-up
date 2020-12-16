2d35af6917f25b6b1e2ba2735209b915012f3fff
Ryoma MATSUMURA <yakitori_che_r.m@icloud.com>
2020-12-17 00:05:01 +0900

    ランキングの実装

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-'use strict';
+'use strict'; //JS strict mode
+
+
+const fs = require('fs') ; //fs = File System
+const readline = require('readline') ; // ファイルを１行ずつ読み込むモジュール
+const rs = fs.createReadStream('./popu-pref.csv') ; 
+const rl = readline.createInterface({input: rs, output: {} }) ; 
+const prefectureDataMap = new Map() ; 
+
+rl.on('line' , lineString => { //lineが発生したら
+    const columns = lineString.split(',') ;
+    const year = parseInt(columns[0]) ;
+    const prefecture = columns[1] ; 
+    const popu = parseInt(columns[3]) ;
+
+    if(year === 2010 || year === 2015){
+        let value = prefectureDataMap.get(prefecture) ; //prefecture から値を取得
+        if(!value){ //もしvalueがnullなら
+            value = {
+                popu10 : 0 ,
+                popu15 : 0 ,
+                change : null 
+            };
+        }
+        if(year === 2010){
+            value.popu10 = popu ;
+        }
+        if( year ===2015){
+            value.popu15 = popu ;
+        }
+
+        prefectureDataMap.set(prefecture , value ) ; 
+
+    }
+}) ;
+
+rl.on('close' , () =>{//rl closeしたら出力
+    for( let [key , value] of prefectureDataMap){//valueの中のkeyについてのループ
+        value.change = value.popu15/value.popu10 ; 
+    }
+    const rankingArray = Array.from(prefectureDataMap).sort((pair1 , pair2) => {//map -> array変換
+        return pair2[1].change - pair1[1].change ; 
+    }) ; 
+    const rankingStrings = rankingArray.map(([key, value]) => {
+        return (
+            key + ': ' + value.popu10 + '=>' + value.popu15 + ' 変化率:' + value.change
+        );
+    });
+    console.log(rankingStrings);
+
+}) ; 
